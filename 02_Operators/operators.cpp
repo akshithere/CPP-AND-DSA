a349d85bd4e2e3952e9654c0e6ccc0959bd83102
akshithere <130065975+akshithere@users.noreply.github.com>
2024-01-01 22:26:54 +0530

    Operators in cpp
    Operators in cpp

--- 02_Operators/operators.cpp
+++ 02_Operators/operators.cpp
@@ -31,7 +31,7 @@ int a,b;
        cout << (!exp1) << endl;  //false */
 
        //Assignment operators
-       
+       /*
        a+=1; // 11
        cout << a << endl;
        a-=1; //10
@@ -41,7 +41,16 @@ int a,b;
        a/=1; //10
        cout << a << endl;
        a%=1; //0
-       cout << a << endl; 
+       cout << a << endl; */
 
+       // Bitwise operator
+       /* bits mein khelte hai isme binary to decimal aane chahiye conversion ab isme konse operators aate hai
+       ~ bitwise complement 1 becomes 0 and 0 becomes 1 eg 1010 becomes 0101
+       << Left Shift basically jitne chahe utne places tak we can shift the bits
+       | Bitwise OR
+       & Bitwise AND
+       ^ Bitwise Exlusive OR          */
+       cout << (a << 1); // a is 10 which is 1010 -> 0101 which is 4+1=5
+3
  return 0;
 }
